3de7f65f3d4048ef19261dc099a913e224582145
Nay Fernandes <cadennay17@gmail.com>
2024-02-10 10:29:42 +0530

    part 1

---
+++ exam_215_new/src/exam_215_new/vegetable.java
@@ -0,0 +1,17 @@
+package exam_215_new;
+
+public class vegetable {
+	String name;
+	vegetable(String name)
+	{
+		this.name=name;
+		
+	}
+	public static void main(String[]args) {
+	//TODO Auto-generated method sub
+	vegetable spinach=new vegetable("spinach");
+	System.out.println("Vegetable is "+spinach.name);
+	}
+	
+
+}
